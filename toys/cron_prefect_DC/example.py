fb59c63fb027e05be2bad247e9210011df088871
James Salvatore <jimmy.c.salvatore@gmail.com>
2023-03-31 16:12:21 -0500

    chore: updated prefect example

--- toys/cron_prefect_DC/example.py
+++ toys/cron_prefect_DC/example.py
@@ -1,19 +1,36 @@
 import time
 
+from prefect import flow
 
+
+@flow
 def short_task() -> int:
     """Run a short task."""
-    return 0
+    return 2
 
+
+@flow
 def long_task() -> int:
     """Run a long task."""
     time.sleep(5)
-    return 0
+    return 3
 
+
+@flow
 def longer_task() -> int:
     """Run a longer task."""
     time.sleep(5)
-    return 0
+    return 5
+
+
+@flow
+def longer_and_long_task() -> int:
+    """Run both longer and long task."""
+    return longer_task() * long_task()
+
 
 if __name__ == "__main__":
-    pass
+    print(short_task())
+    print(long_task())
+    print(longer_task())
+    print(longer_and_long_task())
