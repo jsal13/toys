17835de909f4d7ee00379d73666ddb9679c4679f
James Salvatore <jimmy.c.salvatore@gmail.com>
2023-04-13 21:55:48 -0500

    chore: adds great expectations;

---
+++ toys/code_python/great_expectations/gx_example.py
@@ -0,0 +1,72 @@
+import string
+from dataclasses import dataclass
+
+import great_expectations as gx
+import numpy as np
+import pandas as pd
+
+# The data context will allow us to access a bunch of utility and
+# convenience methods; it's the entry point for GX in the Py API.
+context = gx.get_context()
+
+# Let's make some synthetic data for use in GX.
+
+
+@dataclass
+class Record:
+    """Represent a Record."""
+
+    field_int: int | None
+    field_nonneg_int: int | None
+    field_required_int: int | None
+    field_float: float | None
+    field_str: str | None
+
+
+def create_random_records(n: int, missing_data_percent: float = 0.1) -> list["Record"]:
+    """Create random records."""
+
+    # We purposely put some malformed data in here.
+    field_ints = np.random.randint(-100, 100, size=n)
+    field_nonneg_int = np.random.randint(-10, 100, size=n)
+    field_required_int = np.random.randint(-100, 100, size=n)
+    field_float = np.random.rand(n)
+    field_str = np.random.choice(list(string.ascii_lowercase), size=n, replace=True)
+
+    fields = {
+        "field_int": field_ints,
+        "field_nonneg_int": field_nonneg_int,
+        "field_required_int": field_required_int,
+        "field_float": field_float,
+        "field_str": field_str,
+    }
+
+    df = pd.DataFrame(fields)
+
+    # Create random nulls.
+    df = df.mask(
+        np.random.choice(
+            [True, False],
+            size=df.shape,
+            p=[missing_data_percent, 1 - missing_data_percent],
+        )
+    )
+
+    return df
+
+df = create_random_records(100)
+
+# Create a validator for reading a dataframe.
+# A validator stores Expectations about data it's associated with, and performing introspections on the data.
+
+validator = context.sources.pandas_default.read_dataframe(df)
+
+# Let's do a checkpoint, this allows us to repeat validation.
+
+checkpoint = gx.checkpoint.SimpleCheckpoint(name="quickstart_checkpoint", data_context=context, validator=validator)
+
+# Run the checkpoint.
+checkpoint_result = checkpoint.run()
+
+# Gx compiles this stuff into a nice little data doc and opens a webpage.
+context.build_data_docs()
---
+++ toys/code_python/great_expectations/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python -m http.server
+python /app/toys/code_python/great_expectations/gx_example.py
